0676e84c9738a3aa39f55e0525071dccecf7080e
Bréwen CHENEAU <brewen.cheneau@gmail.com>
2025-03-28 19:00:34 +0100

    Ajout section qui suis je, CV,présentation,compétence

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const headerHamMenuCloseBtn = document.querySelector(
   '.header__main-ham-menu-close'
 )
 const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
-
+ 
 hamMenuBtn.addEventListener('click', () => {
   if (smallMenu.classList.contains('header__sm-menu--active')) {
     smallMenu.classList.remove('header__sm-menu--active')
